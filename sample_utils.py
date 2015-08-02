8192fdab643bd7d3de67f2805f8246e4ba2c6923
Szymon Sidor <szymon.sidor@gmail.com>
2015-08-02 00:52:52 -0700

    Initial commit

---
+++ sample_utils.py
@@ -0,0 +1,14 @@
+def typed_expression(pyp, replacable_expression, replacable_type, code):
+    pyp.indent('if self.dtype == np.int32:')
+    typefixed = code.replace('TYPED_EXPRESSION', '(<%s[int]>(%s))' % (replacable_type, replacable_expression))
+    pyp.indent(typefixed)
+
+    pyp.indent('elif self.dtype == np.float32:')
+    typefixed = code.replace('TYPED_EXPRESSION', '(<%s[float]>(%s))' % (replacable_type, replacable_expression))
+    pyp.indent(typefixed)
+
+    pyp.indent('elif self.dtype == np.float64:')
+    typefixed = code.replace('TYPED_EXPRESSION', '(<%s[double]>(%s))' % (replacable_type, replacable_expression))
+    pyp.indent(typefixed)
+    pyp.indent('else:')
+    pyp.indent('    raise ValueError("Invalid dtype:" + self.dtype + " (should be one of int32, float32, float64)")')
